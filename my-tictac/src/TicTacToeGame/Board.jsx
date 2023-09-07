15ca918e7439de017b4c95e4358b5870d04bab60
Abdullah-Rajpoot <ar0306654>
2023-09-07 12:56:58 +0500

    TicTacToeGame

---
+++ my-tictac/src/TicTacToeGame/Board.jsx
@@ -0,0 +1,35 @@
+"use client"
+import React, { useState } from 'react'
+import Square from './Square';
+
+const Board = () => {
+const [state, setState] = useState(Array(9).fill(null));
+
+console.log("State", state);
+
+  return (
+    <div className='board-container'>
+    <div className='board-row'>
+    <Square/>
+    <Square/>
+    <Square/>
+    </div>
+    <div className='board-row'>
+    <Square/>
+    <Square/>
+    <Square/>
+    </div>
+    <div className='board-row'>
+    <Square/>
+    <Square/>
+    <Square/>
+    </div>
+    </div>
+  );
+};
+
+ 
+ 
+
+
+export default Board;
---
+++ my-tictac/src/TicTacToeGame/Square.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Square = () => {
+  return (
+    <div 
+    style={{ border:"1px solid", 
+     height: "100px", width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center", 
+    }} 
+    className="Square"
+    >
+        <h5>o</h5>
+    </div>
+   
+  );
+};
+
+export default Square;
+
+
