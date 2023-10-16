25a0957e798c296ddc8ec8c6b8e33d761e71b870
Abdullah-Rajpoot <ar0306654>
2023-10-16 21:51:48 +0500

    new1data

--- app/TicTacToeGame/Square.jsx
+++ app/TicTacToeGame/Square.jsx
@@ -1,3 +1,23 @@
+// import React from 'react'
+// import Rood from './rood';
+// const Square = ({onClick,value}) => {
+//   return (
+//     <div
+//     onClick={onClick}
+//      style={{ border:"1px solid", 
+//      height: "100px", width: "100%",
+//       display: "flex",
+//       justifyContent: "center",
+//       alignItems: "center", 
+//     }} 
+//      className='square'>
+//         <h5>{value}</h5>
+//     </div>
+//   )
+// }
+
+// export default Square;
+
 import React from 'react'
 import Rood from './rood';
 const Square = ({onClick,value}) => {
--- app/page.js
+++ app/page.js
@@ -11,3 +11,6 @@ function App(){
   </div>
 }
 export default App;
+
+
+
